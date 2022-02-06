78bc8f0f3c09aa7bfd5d0b8360239c26ff140a23
yoonzet <lalala703@naver.com>
2022-02-06 20:40:35 +0900

    cont 스크롤매직까지함

--- js/script.js
+++ js/script.js
@@ -1,10 +1,69 @@
-$(document).ready(function(){
-    $('.fullpage').fullpage({
+$(document).ready(function () {
+  // 풀페이지
+  $('#fullpage').fullpage({
+    fadingEffect: true,
+  });
 
-    });
-    
+  // 스크롤매직
+  var controller = new ScrollMagic.Controller();
+  // hotnow
+  var ani01 = gsap.from('.hotnow .img-wrap', {
+    opacity: 0,
+    x: -800,
+  });
+  var timeLine = gsap.timeline();
 
-    $(".slider").slick({
-      });
+  timeLine.add(ani01);
+
+  var scene01 = new ScrollMagic.Scene({
+    triggerElement: '.hotnow',
+    triggerHook: 1,
+    offset: 200,
+    duration: "100%",
+  });
+  scene01.setTween(timeLine);
+  scene01.addTo(controller);
+  // scene01.addIndicators();
+
+
+  // dozen
+  var ani02 = gsap.from('.dozen .img-wrap', {
+    opacity: 0,
+    x: 800,
+  });
+  var timeLine = gsap.timeline();
+
+  timeLine.add(ani02);
+
+  var scene02 = new ScrollMagic.Scene({
+    triggerElement: '.dozen',
+    triggerHook: 1,
+    offset: 200,
+    duration: "100%",
+  });
+  scene02.setTween(timeLine);
+  scene02.addTo(controller);
+  // scene02.addIndicators();
+
+
+  // group-order
+  var ani03 = gsap.from('.group-order .img-wrap', {
+    opacity: 0,
+    x: -800,
+  });
+  var timeLine = gsap.timeline();
+
+  timeLine.add(ani03);
+
+  var scene03 = new ScrollMagic.Scene({
+    triggerElement: '.group-order',
+    triggerHook: 1,
+    offset: 200,
+    duration: "100%",
+  });
+  scene03.setTween(timeLine);
+  scene03.addTo(controller);
+  // scene03.addIndicators();
+
+
 });
-
